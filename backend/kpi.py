45b23cfb897808915d3c03fa76e04b9bed648726
codmihaic <codreanumihaic@gmail.com>
2026-05-12 19:13:03 +0300

    adaugare kpi-uri in dashboard si adaptare pentru frontend

--- backend/kpi.py
+++ backend/kpi.py
@@ -70,25 +70,24 @@ def get_tickets_by_priority(db: Session = Depends(get_db)):
 def get_average_resolution_time(db: Session = Depends(get_db)):
     query = text(
     """ 
-        SELECT AVG(DATEDIFF(SECOND, SUBMIT_DATETIME, RESOLVED_DATETIME)) as average_resolution_seconds
-        FROM INCIDENT_TICKETS
-        WHERE RESOLVED_DATETIME is not NULL AND SUBMIT_DATETIME is not NULL
+        SELECT AVG(DATEDIFF(SECOND, SUBMIT_DATETIME, RESOLVED_DATETIME)) as avg_resolution_seconds
+        FROM INCIDENT_TICKETS t
+        WHERE t.RESOLVED_DATETIME is not NULL AND t.SUBMIT_DATETIME is not NULL
     """)
 
     result = db.execute(query).mappings().first()
-    avg_resolution_time = []
-    for row in result:
-        avg_seconds = row["avg_resolution_time"] if row["avg_resolution_time"] is not None else 0
-        avg_resolution_time.append({
-            "team": row["team"] if row["team"] is not None else "Necunoscut",
-            "average_resolution_time_seconds": round(avg_seconds, 2),
-            "average_resolution_time_minutes": round(avg_seconds / 60, 2),
-            "average_resolution_time_hours": round(avg_seconds / 3600, 2)
-        })
+    avg_seconds = result["avg_resolution_seconds"] if result["avg_resolution_seconds"] is not None else 0
+    # avg_resolution_time = [{
+    #     "average_resolution_time_seconds": round(avg_seconds, 2),
+    #     "average_resolution_time_minutes": round(avg_seconds / 60, 2),
+    #     "average_resolution_time_hours": round(avg_seconds / 3600, 2)
+    # }]
+    avg_resolution_time = round(avg_seconds / 3600, 2)
     
     return {
         "label": "Average Resolution Time",
-        "data": avg_resolution_time
+        "data": avg_resolution_time,
+        "unit": "h"
     }
     
 
@@ -99,7 +98,7 @@ def get_unresolved_percentage(db: Session = Depends(get_db)):
     """
         SELECT COUNT(*) as unresolved_count
         FROM INCIDENT_TICKETS t
-        JOIN STATUSES s ON t.STATUS = s.STATUS_ID
+        JOIN STATUSES s ON t.STATUS_ID = s.STATUS_ID
         WHERE s.STATUS_NAME != 'Closed' AND s.STATUS_NAME != 'Resolved'
     """)
 
@@ -117,7 +116,8 @@ def get_unresolved_percentage(db: Session = Depends(get_db)):
     percentage = round((unresolved_count / total_tickets) * 100, 2) if total_tickets > 0 else 0.00
     return {
         "label": "Unresolved Tickets Percentage:",
-        "value": percentage
+        "value": percentage,
+        "unit": "%"
     }
 
 # KPI 6: numarul total de statusuri rezolvate in procent din total: - card
@@ -127,7 +127,7 @@ def get_resolved_percentage(db: Session = Depends(get_db)):
     """
         SELECT COUNT(*) as resolved_count
         FROM INCIDENT_TICKETS t
-        JOIN STATUSES s ON t.STATUS = s.STATUS_ID
+        JOIN STATUSES s ON t.STATUS_ID = s.STATUS_ID
         WHERE s.STATUS_NAME = 'Closed' OR s.STATUS_NAME = 'Resolved'
     """)
 
@@ -145,7 +145,8 @@ def get_resolved_percentage(db: Session = Depends(get_db)):
     percentage = round((resolved_count / total_tickets) * 100, 2) if total_tickets > 0 else 0.00
     return {
         "label": "Resolved Tickets Percentage:",
-        "value": percentage
+        "value": percentage,
+        "unit": "%"
     }
 
 # KPI 7: numarul total de statusuri cu timpul de lucru depasit in procent din total: - card
@@ -173,7 +174,8 @@ def get_overdue_percentage(db: Session = Depends(get_db)):
     percentage = round((overdue_count / total_tickets) * 100, 2) if total_tickets > 0 else 0.00
     return {
         "label": "Overdue Tickets Percentage:",
-        "value": percentage
+        "value": percentage,
+        "unit": "%"
     }
 
 # KPI 8: numarul total de tickete pe echipa: - bar chart
@@ -217,20 +219,21 @@ def get_average_resolution_time_per_team(db: Session = Depends(get_db)):
         avg_seconds = row["avg_resolution_time"] if row["avg_resolution_time"] is not None else 0
         avg_resolution_time_per_team.append({
             "team": row["team"] if row["team"] is not None else "Necunoscut",
-            "average_resolution_time_seconds": round(avg_seconds, 2),
-            "average_resolution_time_minutes": round(avg_seconds / 60, 2),
+            # "average_resolution_time_seconds": round(avg_seconds, 2),
+            # "average_resolution_time_minutes": round(avg_seconds / 60, 2),
             "average_resolution_time_hours": round(avg_seconds / 3600, 2)
         })
     
     return {
         "label": "Average Resolution Time per Team:",
-        "data": avg_resolution_time_per_team
+        "data": avg_resolution_time_per_team,
+        "unit": "h"
     }
     
 
 # KPI 10: tickete pe categorie (tier 1, tier 2, tier 3): - pie chart pentru fiecare
 @router.get("/tickets/category/tier-1")
-def get_tickets_by_category(db: Session = Depends(get_db)):
+def get_tickets_by_category_1(db: Session = Depends(get_db)):
     query = text(
     """
         SELECT t.CATEGORY_TIER_1 as category1, COUNT(*) as ticket_count
@@ -241,14 +244,14 @@ def get_tickets_by_category(db: Session = Depends(get_db)):
     result = db.execute(query).mappings().all()
     return [
         {
-            "Category Tier 1": row["category1"] if row["category1"] is not None else "Necunoscut",
+            "category": row["category1"] if row["category1"] is not None else "Necunoscut",
             "count": row["ticket_count"]
         }
         for row in result
     ]
 
 @router.get("/tickets/category/tier-2")
-def get_tickets_by_category(db: Session = Depends(get_db)):
+def get_tickets_by_category_2(db: Session = Depends(get_db)):
     query = text(
     """
         SELECT t.CATEGORY_TIER_2 as category2, COUNT(*) as ticket_count
@@ -259,14 +262,14 @@ def get_tickets_by_category(db: Session = Depends(get_db)):
     result = db.execute(query).mappings().all()
     return [
         {
-            "Category Tier 2": row["category2"] if row["category2"] is not None else "Necunoscut",
+            "category": row["category2"] if row["category2"] is not None else "Necunoscut",
             "count": row["ticket_count"]
         }
         for row in result
     ]
 
 @router.get("/tickets/category/tier-3")
-def get_tickets_by_category(db: Session = Depends(get_db)):
+def get_tickets_by_category_3(db: Session = Depends(get_db)):
     query = text(
     """
         SELECT t.CATEGORY_TIER_3 as category3, COUNT(*) as ticket_count
@@ -277,7 +280,7 @@ def get_tickets_by_category(db: Session = Depends(get_db)):
     result = db.execute(query).mappings().all()
     return [
         {
-            "Category Tier 3": row["category3"] if row["category3"] is not None else "Necunoscut",
+            "category": row["category3"] if row["category3"] is not None else "Necunoscut",
             "count": row["ticket_count"]
         }
         for row in result
@@ -285,62 +288,51 @@ def get_tickets_by_category(db: Session = Depends(get_db)):
 
 # KPI ...
 
-# Dashboard cu primele 3 KPI-urile:
+# Dashboard cu primele 3 KPI-uri:
 @router.get("/dashboard")
 def get_kpi_dashboard(db: Session = Depends(get_db)):
-    total_tickets_query = text(
-    """
-        SELECT COUNT(*) as total_tickets
-        FROM INCIDENT_TICKETS
-    """)
+    total_tickets = get_all_tickets(db)
+    tickets_by_status = get_tickets_by_status(db)
+    tickets_by_priority = get_tickets_by_priority(db)
+    avg_resolution_result = get_average_resolution_time(db)
+    avg_resolution_time = avg_resolution_result["data"]
 
-    status_query = text(
-    """
-        SELECT 
-            s.STATUS_NAME as status,
-            COUNT(*) as ticket_count
-        FROM INCIDENT_TICKETS t
-        JOIN STATUSES s ON t.STATUS_ID = s.STATUS_ID
-        GROUP BY s.STATUS_NAME
-    """)
+    unresolved_tickets = get_unresolved_percentage(db)
+    resolved_tickets = get_resolved_percentage(db)
+    overdue_tickets = get_overdue_percentage(db)
 
-    priority_query = text(
-    """
-        SELECT 
-            p.PRIORITY_NAME as priority,
-            COUNT(*) as ticket_count
-        FROM INCIDENT_TICKETS t
-        JOIN PRIORITIES p ON t.PRIORITY_ID = p.PRIORITY_ID
-        GROUP BY p.PRIORITY_NAME
-        ORDER BY ticket_count DESC
-    """)
+    tickets_per_team_result = get_tickets_per_team(db)
+    tickets_per_team = tickets_per_team_result["data"]
+    avg_res_time_per_team_result = get_average_resolution_time_per_team(db)
+    avg_res_time_per_team = avg_res_time_per_team_result["data"]
 
-    total_tickets_result = db.execute(total_tickets_query).mappings().first()
-    status_result = db.execute(status_query).mappings().all()
-    priority_result = db.execute(priority_query).mappings().all()
+    category_tier_1 = get_tickets_by_category_1(db)
+    category_tier_2 = get_tickets_by_category_2(db)
+    category_tier_3 = get_tickets_by_category_3(db)
 
     return {
-        "total_tickets": 
-        {
-            "label": "Total Tickets",
-            "value": total_tickets_result["total_tickets"]
+        "total_tickets": total_tickets,
+        "tickets_by_status": tickets_by_status,
+        "tickets_by_priority": tickets_by_priority,
+
+        "avg_res_time": {
+            "label": avg_resolution_result["label"],
+            "value": avg_resolution_time,
+            "unit": avg_resolution_result["unit"]
         },
 
-        "tickets_by_status": 
-        [
-            {
-                "status": row["status"] if row["status"] is not None else "Necunoscut",
-                "count": row["ticket_count"]
-            }
-            for row in status_result
-        ],
+        "unresolved_tickets": unresolved_tickets,
+        "resolved_tickets": resolved_tickets,
+        "overdue_tickets": overdue_tickets,
 
-        "tickets_by_priority": 
-        [
-            { 
-                "priority": row["priority"] if row["priority"] is not None else "Necunoscut",
-                "count": row["ticket_count"]
-            }
-            for row in priority_result
-        ]
+        "tickets_per_team": tickets_per_team,
+        "avg_res_time_per_team": {
+            "label": avg_res_time_per_team_result["label"],
+            "data": avg_res_time_per_team,
+            "unit": "h"
+        },
+
+        "category_tier_1": category_tier_1,
+        "category_tier_2": category_tier_2,
+        "category_tier_3": category_tier_3
     }
